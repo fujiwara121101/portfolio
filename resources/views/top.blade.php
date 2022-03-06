4c78b70a1f0578221389b08a81b7f5c46c21ae33
fujiwara121101 <fujiwara121101@gmail.com>
2022-03-06 11:39:50 +0900

    変更箇所

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 <h1>ポーカー Game to Live</h1>
 
+<div class="profiles/show">[<a href="profiles/create">プロフィール登録</a>]</div>
 <div class="search">[<a href="profiles/search">ユーザー検索</a>]</div>
+<div class="stores">[<a href="/stores">店舗情報</a>]</div>
+<div class="manners">[<a href="/manners">マナー集</a>]</div>
+
 
 
 @endsection
